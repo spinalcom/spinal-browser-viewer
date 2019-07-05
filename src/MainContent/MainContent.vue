9cc8cf73fe434f504ee95e9d443e368f6893cf0e
LorisC <loris@ciprin.fr>
2019-07-05 10:40:06 +0200

    add multi maquette support

--- src/MainContent/MainContent.vue
+++ src/MainContent/MainContent.vue
@@ -1,18 +1,53 @@
 <template>
-  <div style="overflow: hidden">
-    <forge-content />
-  </div>
+    <div style="overflow: hidden" class="spinal-browser-viewer-integration">
+
+
+        <spinal-forge-viewer-vue class="spinal-forge-viewer"
+                                 :onInitialize="onInitialize"
+                                 :model-property="{path : '/models/Resource/3D View/{3D} 341878/{3D}.svf' }"
+                                 :headless="false"/>
+        <div id="graph-manager-side"></div>
+    </div>
 </template>
 
 <script>
-import ForgeContent from "./ForgeContent.vue";
-import ModelExplorer from "./ModelExplorer/ModelExplorer.vue";
-export default {
-  name: "app",
-  components: { ForgeContent, ModelExplorer },
-  data() {
-    return {};
-  },
-  mounted() {}
-};
+  import Vue from "vue";
+
+  import { SpinalForgeViewerVue } from "spinal-forge-viewer-vue";
+
+
+  export default {
+    name: "app",
+    components: { SpinalForgeViewerVue },
+    data() {
+      return {};
+    },
+    methods: {
+      onInitialize: function ( viewerManager ) {
+        Vue.prototype.$ForgeViewer.viewer = viewerManager.viewer;
+        Vue.prototype.$ForgeViewer.viewerManager = viewerManager;
+        Vue.prototype.$ForgeExtentionManager.viewer = viewerManager.viewer;
+
+      }
+    },
+
+    mounted() {}
+  };
 </script>
+<style>
+    .graph-manager-side {
+        width: 20%;
+    }
+
+    .spinal-forge-viewer {
+        width: 70%;
+        flex-grow: 3;
+        position: relative;
+    }
+
+    .spinal-browser-viewer-integration {
+        display: flex;
+        height: 100%;
+    }
+</style>
+
