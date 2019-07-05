215107b878bf032231aaa8ca9d39e4d56fdf5a5e
LorisC <loris@ciprin.fr>
2019-07-05 10:39:40 +0200

    load directly extension

--- src/MainContent/ForgeExtentionManager.js
+++ src/MainContent/ForgeExtentionManager.js
@@ -1,13 +1,17 @@
 class ForgeExtentionManager {
   constructor() {
     this.extentions = [];
+    this.viewer = null;
   }
-
+  
   getExtentions() {
     return this.extentions;
   }
-
+  
   addExtention(name) {
+    if (this.viewer !== null)
+      this.viewer.loadExtension(name, {});
+    
     this.extentions.push(name);
   }
 }
