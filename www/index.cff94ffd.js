89b4e310507d92ed406db9a793bbe7681f1f1562
Laurent HA TAM DAN <l.hatamdan@spinalcom.com>
2023-10-10 17:25:57 +0200

    update generate spatial

---
+++ www/index.cff94ffd.js
@@ -0,0 +1,165 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+(function (modules, entry, mainEntry, parcelRequireName, globalName) {
+  /* eslint-disable no-undef */
+  var globalObject =
+    typeof globalThis !== 'undefined'
+      ? globalThis
+      : typeof self !== 'undefined'
+      ? self
+      : typeof window !== 'undefined'
+      ? window
+      : typeof global !== 'undefined'
+      ? global
+      : {};
+  /* eslint-enable no-undef */
+
+  // Save the require from previous bundle to this closure if any
+  var previousRequire =
+    typeof globalObject[parcelRequireName] === 'function' &&
+    globalObject[parcelRequireName];
+
+  var cache = previousRequire.cache || {};
+  // Do not use `require` to prevent Webpack from trying to bundle this call
+  var nodeRequire =
+    typeof module !== 'undefined' &&
+    typeof module.require === 'function' &&
+    module.require.bind(module);
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire =
+          typeof globalObject[parcelRequireName] === 'function' &&
+          globalObject[parcelRequireName];
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error("Cannot find module '" + name + "'");
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = (cache[name] = new newRequire.Module(name));
+
+      modules[name][0].call(
+        module.exports,
+        localRequire,
+        module,
+        module.exports,
+        this
+      );
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x) {
+      var res = localRequire.resolve(x);
+      return res === false ? {} : newRequire(res);
+    }
+
+    function resolve(x) {
+      var id = modules[name][1][x];
+      return id != null ? id : x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [
+      function (require, module) {
+        module.exports = exports;
+      },
+      {},
+    ];
+  };
+
+  Object.defineProperty(newRequire, 'root', {
+    get: function () {
+      return globalObject[parcelRequireName];
+    },
+  });
+
+  globalObject[parcelRequireName] = newRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (mainEntry) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(mainEntry);
+
+    // CommonJS
+    if (typeof exports === 'object' && typeof module !== 'undefined') {
+      module.exports = mainExports;
+
+      // RequireJS
+    } else if (typeof define === 'function' && define.amd) {
+      define(function () {
+        return mainExports;
+      });
+
+      // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+})({"9TWIG":[function(require,module,exports) {
+require("3a4df3751dab39f0").register(JSON.parse('{"13Gz7":"index.e548bb63.js","8JuqC":"spinal-env-viewer-context-geographic.fd88acde.js","39OE9":"spinal-env-viewer-context-geographic.56a92a8c.js","bBy4z":"spinal-env-viewer-plugin-circular-menu.4281328a.js","8pHxT":"spinal-env-viewer-plugin-circular-menu.1f4d61a7.css","iI6OK":"spinal-env-viewer-plugin-dashboard-standard.159ee5de.js","aJfGH":"spinal-env-viewer-plugin-dashboard-standard.1505fc00.css","jEDS6":"spinal-env-viewer-plugin-dashboard-standard.e126eae2.js","8EHzY":"spinal-env-viewer-plugin-documentation.38928328.js","asnms":"spinal-env-viewer-plugin-documentation.117365b5.css","dqfTF":"dist.749081fa.js","bc14C":"spinal-env-viewer-plugin-documentation.d8e24179.js","aLZ2D":"spinal-env-viewer-plugin-documentation.5c8b72db.js","iOKvY":"spinal-env-viewer-plugin-documentation.a1bc9267.js","jzdKR":"spinal-env-viewer-plugin-generate_geographic_context.5e7d6836.js","lC3QZ":"spinal-env-viewer-plugin-generate_geographic_context.4c446a8d.css","U3Pvs":"spinal-env-viewer-plugin-graph-manager.ec66f1b0.js","je5Rj":"spinal-env-viewer-plugin-graph-manager.09971ede.css","e3cDp":"spinal-env-viewer-plugin-graph-manager.ecf2be51.js","h5luG":"spinal-env-viewer-vue-components-lib.3778cb57.js","5FblX":"spinal-env-viewer-plugin-graph_export.9e68279f.js","d2HfA":"spinal-env-viewer-plugin-node-inspector.e1a87d51.js","lCVlm":"spinal-env-viewer-plugin-node-inspector.7ec4e045.css","9DGaa":"spinal-env-viewer-plugin-scene.667b98ce.js","4Kpx8":"spinal-env-viewer-plugin-scene.64258481.css","cpAB2":"spinal-env-viewer-plugin-scene.0aa1677e.js","kP5i0":"spinal-env-viewer-plugin-spinal-linker.9bd581f2.js","kKo5J":"spinal-env-viewer-plugin-spinal-linker.23c64f41.css","jHWka":"spinal-env-viewer-plugin-standard_button.7d400cc2.js","h8hP2":"spinal-env-viewer-plugin-standard_button.e637ba35.css","dNyHB":"spinal-env-viewer-plugin-standard_button.b3d4e8a2.js","kHEfo":"spinal-env-viewer-plugin-version.0e5961e8.js","3CW71":"spinal-env-viewer-plugin-version.f0f20f81.css","9ktZ0":"spinal-env-viewer-room-manager.cebd4733.js","3Abh7":"spinal-env-viewer-room-manager.692a8af1.css","bgmq4":"dist.bf23f9d4.js","b8kSd":"spinal-env-viewer-room-manager.cc073a09.js","zluoj":"spinal-env-viewer-room-manager.56afe643.js","9paV8":"spinal-env-viewer-window-selection.558f3883.js","3Qckv":"spinal-env-viewer-plugin-item_model_selector.34e6ede3.js","cGCKR":"spinal-env-viewer-plugin-task.6fe88270.js","6BWFL":"en.es.a0ced837.js","aRBsc":"ar.es.83d0de3a.js","4OxpP":"bg.es.2c047715.js","04W38":"bn.es.eae2f888.js","4lFjx":"bs.es.68343575.js","9ZjgO":"ca.es.c7165aff.js","aEDym":"cs.es.2906b4e8.js","eLpfs":"da.es.ed65619e.js","kT0El":"de.es.d495a489.js","5BN10":"el.es.17977e38.js","grPG9":"es.es.927ad0fc.js","Kk1gA":"fa.es.ac0b8066.js","fcNpg":"fr.es.a2f56a47.js","hDRR3":"he.es.f51056bd.js","1eD2g":"hr.es.463406a3.js","7CAEU":"hu.es.fb365b2a.js","8aWF0":"id.es.f1f2e5a3.js","hJdUj":"is.es.75862b19.js","1chdk":"it.es.7b10841c.js","g1Ncl":"ja.es.7d55f664.js","3RGrr":"ka.es.09fe616f.js","8VUYv":"ko.es.6cc8ca36.js","52U5g":"lt.es.20f90eef.js","55LUS":"mn.es.a4c1b037.js","aqTm9":"nl.es.5b7a3249.js","aoOeE":"no.es.e0aa678c.js","6Sflq":"pl.es.578d5948.js","7ggR2":"pt-br.es.9d32142f.js","5O3Zi":"ro.es.17fb3c15.js","5dyzZ":"ru.es.350bddd4.js","9W8C8":"sk.es.c1e1f107.js","9T9u0":"sl.es.ea13a227.js","bzj8N":"sq.es.12d2ad66.js","fqKtT":"sr.es.ff00c93c.js","8y07x":"sv.es.fcfbc4c7.js","aVDJh":"tr.es.41ce6743.js","feeHb":"uk.es.7da6eda0.js","5kSTG":"vi.es.e922ed2d.js","fiofN":"zh-cn.es.359160b8.js","3UlqZ":"zh-hk.es.847fe938.js","hAcVI":"drag-and-drop.es.157d4e7d.js","hK5NH":"spinal-env-viewer-plugin-task.2ca69b9f.css","4QBSt":"spinal-env-viewer-plugin-graph_viewer.4d2aabed.js","f4stD":"spinal-env-viewer-plugin-network-tree.c7f30ca1.js","jowb9":"link-edit.90db4208.svg","5TG4M":"slash.3495a882.svg","lW8lT":"spinal-env-viewer-plugin-network-tree.c660c5b0.css","dtV9E":"spinal-env-viewer-plugin-network-tree.bfd82561.js","89uEu":"spinal-env-viewer-plugin-network-tree.78fd661e.js","fPoTk":"spinal-env-viewer-plugin-network-tree.7ca87c82.js","fyPkM":"spinal-env-viewer-plugin-network-tree.5804e409.js","9xAul":"spinal-env-viewer-plugin-analytics.787020d0.js","fcMVz":"spinal-env-viewer-plugin-analytics.9d161716.css","alD19":"spinal-env-viewer-plugin-control-endpoint.ff291cc6.js","efO7k":"spinal-env-viewer-plugin-control-endpoint.9b635caf.css","fdCK4":"spinal-env-viewer-plugin-upload.1f8aefd1.js","iiE6M":"spinal-env-viewer-plugin-upload.923b9d37.css","kdcfB":"spinal-env-viewer-plugin-bacnet-manager.da1786fc.js","5w9u5":"add.72eb4328.svg","i7si1":"spinal-env-viewer-plugin-bacnet-manager.f9f0b918.css","jZDRs":"spinal-env-viewer-plugin-device_profile.8d4bb78c.js","lkP0e":"spinal-env-viewer-plugin-device_profile.c61e5568.css","7bsDr":"spinal-env-viewer-plugin-park-management.903a0ecf.js","4dpV1":"spinal-env-viewer-plugin-park-management.e6290f65.css","fFA7l":"spinal-env-viewer-service.38f5a7c6.js","dqMpD":"device.44c78409.svg","6Kt0u":"light.5702a1d6.svg","3CrKh":"circle.d541e395.svg","jkRjr":"remote_controller.9c72b548.svg","fR1yt":"windows_contact.23de5929.svg","8dkej":"fan_coil.0ed5e1ca.svg","kklY8":"blind.918fc5a2.svg","laTnp":"spinal-env-viewer-service.89a19e94.js","baVAZ":"spinal-env-viewer-standard-attributs.fe576490.js","aDilA":"spinal-env-viewer-plugin-endpoint_chart_viewer.195ce275.js","kIihV":"spinal-env-viewer-plugin-endpoint_chart_viewer.65763c6d.css","5akQs":"spinal-env-viewer-plugin-dashboard-panel.2f34f007.js","61boP":"spinal-env-viewer-plugin-dashboard-panel.bfac85b8.css","9Di3k":"spinal-env-viewer-plugin-filter.32691028.js","35FJ3":"spinal-env-viewer-plugin-filter.1a88ec3a.css","hZSNQ":"spinal-env-viewer-plugin-ticket.126b0323.js","bM04W":"spinal-env-viewer-plugin-ticket.047a67d1.css","5JlfQ":"spinal-env-viewer-plugin-ticket.1a00255c.js","6qSiH":"spinal-env-viewer-plugin-attribute-manager.f3383060.js","lUPzn":"spinal-env-viewer-plugin-attribute-manager.39331bc7.css","82pYl":"spinal-env-viewer-plugin-organ_ticket_mission.634cdab9.js","6pcPk":"spinal-env-viewer-plugin-organ_ticket_mission.7a1470e6.css","dhjCb":"spinal-env-viewer-plugin-generate-spatial-reference.916c35ae.js","jKdXZ":"spinal-env-viewer-plugin-generate-spatial-reference.0203f7bb.css","cfu5v":"index.49d08420.css"}'));
+
+},{"3a4df3751dab39f0":"gS3k4"}],"gS3k4":[function(require,module,exports) {
+"use strict";
+var mapping = {};
+function register(pairs) {
+    var keys = Object.keys(pairs);
+    for(var i = 0; i < keys.length; i++)mapping[keys[i]] = pairs[keys[i]];
+}
+function resolve(id) {
+    var resolved = mapping[id];
+    if (resolved == null) throw new Error("Could not resolve bundle with id " + id);
+    return resolved;
+}
+module.exports.register = register;
+module.exports.resolve = resolve;
+
+},{}]},["9TWIG"], null, "parcelRequire02e5")
+
+//# sourceMappingURL=index.cff94ffd.js.map
